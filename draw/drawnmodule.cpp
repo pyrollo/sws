5a5979add7efb9fa7db4fd7d54a2832e4e5ebbb6
Pierre-Yves Rollo <dev@pyrollo.com>
2021-09-12 20:28:22 +0200

    License code under GPL V3

--- draw/drawnmodule.cpp
+++ draw/drawnmodule.cpp
@@ -1,3 +1,21 @@
+/*
+Short Waves System - A numeric modular synthetizer
+Copyright (C) 2021 Pierre-Yves Rollo <dev@pyrollo.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "drawnitem.h"
 #include "drawnmodule.h"
 #include "drawnschema.h"
